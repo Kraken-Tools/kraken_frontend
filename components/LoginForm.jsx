b4a14a1a13f878ac9c2859309504c64d27f33079
MatheusTurra <matheusfr.318@gmail.com>
2022-07-28 19:20:50 -0300

    _document configurado e novo sistema de redirecionamento

--- components/LoginForm.jsx
+++ components/LoginForm.jsx
@@ -2,6 +2,13 @@ import CustomInput from "../components/Layout/CustomInput";
 
 export default function LoginForm() {
   
+  async function oauthLogin() {
+    const response = await fetch("http://localhost:8000/login/google");
+    const redirectUrl = await response.json();
+    window.location.assign(redirectUrl.target_url);
+  }
+  
+
   return (
     <form className="bg-secondary-background text-white py-10 px-10">
       <section className="flex flex-col gap-5">
@@ -15,8 +22,13 @@ export default function LoginForm() {
       <hr className="my-3 border-main-background"/>
       <div className="flex justify-center items-center">
         <h3>Ou entre com o </h3>
-        <button className="bg-blue-500 p-2 mx-5">Google</button>
+        <button 
+          type="button"
+          onClick={oauthLogin} 
+          className="bg-blue-500 p-2 mx-5">
+            Google
+          </button>
       </div>
     </form>
-);
+  );
 }
--- pages/login.jsx
+++ pages/login.jsx
@@ -1,20 +1,24 @@
+import Script from 'next/script'
 import LoginForm from "../components/LoginForm";
 
 export default function Login() {
   
   return (
-    <div className="min-h-full bg-main-background">
-      <div className="flex flex-row justify-center min-h-screen items-center">
-        <div className="flex p-8 justify-center w-full max-w-5xl">
-          <div className="grid md:grid-cols-2 items-center justify-center gap-10">
-            <div className="flex flex-col text-white">
-              <h1 className="text-4xl text-main-highlight-color font-bold">Kraken</h1>
-              <h1 className="text-5xl">Faça seu login na API</h1>
+    <>
+      <div className="min-h-full bg-main-background">
+        <div className="flex flex-row justify-center min-h-screen items-center">
+          <div className="flex p-8 justify-center w-full max-w-5xl">
+            <div className="grid md:grid-cols-2 items-center justify-center gap-10">
+              <div className="flex flex-col text-white">
+                <h1 className="text-4xl text-main-highlight-color font-bold">Kraken</h1>
+                <h1 className="text-5xl">Faça seu login na API</h1>
+              </div>
+                <LoginForm />
             </div>
-              <LoginForm />
           </div>
         </div>
       </div>
-    </div>
+      <Script src="https://apis.google.com/js/platform.js" async defer/>
+    </>
   );
 }
