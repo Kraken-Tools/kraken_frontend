07189e9578fd112f20e36702e82a16583b84e9be
MatheusTurra <matheusfr.318@gmail.com>
2022-09-07 20:32:22 -0300

    Criar botão de login do google
    - Hook useEffect utilizado para a inicialização das configurações da api do google.
    - Módulo jwt-decode instalado para acessar os dados do usuário retornado do google (link do módulo: https://www.npmjs.com/package/jwt-decode).
    - Tag Script importada do next sendo implementada com a stragegy beforeIntercative para carregamento do script no início da execução da página (dentro da tag head do html)
    .

--- pages/login.jsx
+++ pages/login.jsx
@@ -1,34 +1,61 @@
+import Script from "next/script";
+import { useEffect } from "react";
+import jwtDecode from "jwt-decode";
+
 import CustomInput from "../components/Layout/CustomInput";
 
+const googleClientId = "204455769773-8qvdfjklpitjbrg1ni3qgr9o3lrism0k.apps.googleusercontent.com";
+
 export default function Login() {
+  useEffect(() => {
+    google.accounts.id.initialize({
+      client_id: googleClientId,
+      callback: handleGoogleOauthResponse
+    });
+
+    google.accounts.id.renderButton(
+      document.getElementById("googleSignInButton"),
+      {theme: "dark", size: "medium"}
+    );
+  }, []);
   
+  function handleGoogleOauthResponse(googleResponse) {
+    const userData = jwtDecode(googleResponse.credential);
+  }
+
   return (
-    <div className="min-h-full bg-main-background">
-      <div className="flex flex-row justify-center min-h-screen items-center">
-        <div className="flex p-8 justify-center w-full max-w-5xl">
-          <div className="grid md:grid-cols-2 items-center justify-center gap-10">
-            <div className="flex flex-col text-white">
-              <h1 className="text-4xl text-main-highlight-color font-bold">Kraken</h1>
-              <h1 className="text-5xl">Faça seu login na API</h1>
+    <>
+      <Script 
+        src="https://accounts.google.com/gsi/client"
+        async={true} 
+        defer={true} 
+        strategy="beforeInteractive"/>
+      <div className="min-h-full bg-main-background">
+        <div className="flex flex-row justify-center min-h-screen items-center">
+          <div className="flex p-8 justify-center w-full max-w-5xl">
+            <div className="grid md:grid-cols-2 items-center justify-center gap-10">
+              <div className="flex flex-col text-white">
+                <h1 className="text-4xl text-main-highlight-color font-bold">Kraken</h1>
+                <h1 className="text-5xl">Faça seu login na API</h1>
+              </div>
+              <form className="bg-secondary-background text-white py-10 px-10">
+                <section className="flex flex-col gap-5">
+                  <CustomInput label="E-mail" name="email" placeholder="email@domínio.com" type="email"/>
+                  <CustomInput label="Senha" name="password" placeholder="Mínimo 8 caracteres" type="password"/>
+                </section>
+                <a className="inline-block text-main-highlight-color my-2 cursor-pointer font-black">Esqueci minha senha</a>
+                <div className="">
+                  Não tem uma conta? <a>Registre-se</a>
+                </div>
+                <hr className="my-3 border-main-background"/>
+                <div className="flex justify-center items-center">
+                  <div id="googleSignInButton"/>
+                </div>
+              </form>
             </div>
-            <form className="bg-secondary-background text-white py-10 px-10">
-              <section className="flex flex-col gap-5">
-                <CustomInput label="E-mail" name="email" placeholder="email@domínio.com" type="email"/>
-                <CustomInput label="Senha" name="password" placeholder="Mínimo 8 caracteres" type="password"/>
-              </section>
-              <a className="inline-block text-main-highlight-color my-2 cursor-pointer font-black">Esqueci minha senha</a>
-              <div className="">
-                Não tem uma conta? <a>Registre-se</a>
-              </div>
-              <hr className="my-3 border-main-background"/>
-              <div className="flex justify-center items-center">
-                <h3>Ou entre com o </h3>
-                <button className="bg-blue-500 p-2 mx-5">Google</button>
-              </div>
-            </form>
           </div>
         </div>
       </div>
-    </div>
+    </>
   );
 }
